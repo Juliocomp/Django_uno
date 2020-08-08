319ff6ba69760a76bfb53d10e19c25b47f77a03e
juliocomp <jules200102@gmail.com>
2020-08-08 13:43:49 -0500

    make it better

--- Dark_light/settings.py
+++ Dark_light/settings.py
@@ -86,7 +86,7 @@ import dj_database_url
 from decouple import config
 
 DATABASES={
-    'default': dj_databases_url.config(
+    'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
